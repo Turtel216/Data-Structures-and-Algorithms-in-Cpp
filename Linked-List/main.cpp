0671f242c57403388b6c5ee773ad3e5a63f0636f
Turtel216 <papadimitrios216@gmail.com>
2024-05-23 11:17:15 +0300

    Created LinkedList. Not finished

--- Linked-List/main.cpp
+++ Linked-List/main.cpp
@@ -1,6 +1,48 @@
+#include <iostream>
+#include <string>
+
+template <typename T>
+class Node 
+{
+private:
+  T payload;
+
+public:
+  Node* next;
+  Node* previous;
+
+  Node(T data) noexcept
+    :payload(data) {}
+
+  void setPayload(T data) { payload = data; }
+  T getPayload() { return payload; }
+};
+
+template <typename T>
+class LinkedList
+{
+private:
+  Node<T>* current;
+
+public:
+  void addToList(Node<T>* node)
+  {
+    if(current == nullptr)
+      current = node;
+
+    node->previous = current;
+    node->next = nullptr;
+  }
+};
 
 
 int main () {
-  
+  Node<int>* node1 = new Node(1);
+  Node<int>* node2 = new Node(1);
+  Node<int>* node3 = new Node(1);
+
+  LinkedList<int>* list = new LinkedList<int>();
+  list->addToList(node1);
+
   return 0;
 }
