e9053de9302ad3979b381da33e7b713706a43b5a
Turtel216 <papadimitrios216@gmail.com>
2024-05-23 14:12:50 +0300

    Implemented callback method in LinkedList

--- Linked-List/main.cpp
+++ Linked-List/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-#include <string>
+// #include <iostream>
+// #include <string>
 
 template <typename T>
 class Node 
@@ -33,8 +33,16 @@ public:
     node->previous = current;
     node->next = nullptr;
   }
+  void sort(bool(*comparator)(T, T))
+  {
+    //TO stuff
+  }
 };
 
+bool compareInts(int a, int b)
+{
+  return a<b;
+}
 
 int main () {
   Node<int>* node1 = new Node(1);
@@ -43,6 +51,7 @@ int main () {
 
   LinkedList<int>* list = new LinkedList<int>();
   list->addToList(node1);
+  list->sort(&compareInts);
 
   return 0;
 }
