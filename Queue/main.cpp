8a6c8ee70b1b4fc97458a363b38424bebd2df457
Turtel216 <papadimitrios216@gmail.com>
2024-05-29 15:24:51 +0300

    Updated Queue

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -20,7 +20,27 @@ public:
     this->array =  new T[this->capacity];
   }
 
+  // Getters
   int getFront() const noexcept { return front; }
   int getRear() const noexcept { return rear; }
   size_t getSize() const noexcept { return size; }
+
+  // Method that returns true if the queue is full
+  bool isFull() const noexcept { return this->size == this->capacity; }
+
+  // Method that returns true if the queue is empty
+  bool isEmpty() const noexcept { return this->size == 0; }
+
+  void add(T value) noexcept
+  {
+    // Check if the queue is full
+    if (isFull())
+    {
+      std::cout << "The Queue is full, no element can be added" << std::endl;
+      return;
+    }
+
+    rear = (rear + 1) % capacity;
+    size++;
+  }
 };
