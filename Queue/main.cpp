1b0cdaa821db6f8fca3deb84c0982ff2e7fb8b87
Turtel216 <papadimitrios216@gmail.com>
2024-05-29 21:34:35 +0300

    Added queue and dequeue item

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -31,7 +31,20 @@ public:
   // Method that returns true if the queue is empty
   bool isEmpty() const noexcept { return this->size == 0; }
 
-  void add(T value) noexcept
+  // display the content of the queue
+  void display() const noexcept
+  {
+    if(isEmpty())
+      std::cout << "List is empty" << std::endl;
+
+    // Iterate over the list and print each element
+    for(int i = 0; i < size; i++)
+      std::cout << array[i] << std::endl;
+
+  }
+
+  // adds a new item to the queue
+  void add(T item) noexcept
   {
     // Check if the queue is full
     if (isFull())
@@ -41,6 +54,30 @@ public:
     }
 
     rear = (rear + 1) % capacity;
+    array[rear] = item;
     size++;
   }
+
+  T dequeue()
+  {
+    if (isEmpty())
+    {
+      std::cout << "The Queue is empty, no element can be added" << std::endl;
+      return NULL; //TODO throw exception
+    }
+
+    // get front item
+    auto item = array[front];
+    // set new front
+    front = (array[front + 1]) % capacity;
+    size--;
+    return item;
+
+  }
 };
+
+
+int main()
+{
+  Queue<int>* queue = new Queue<int>(5);
+}
